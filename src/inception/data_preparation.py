f5d015b2cac650f377d583987bcc833a9e055713
sbhrwl <sabharwalonnet@gmail.com>
2021-07-29 18:39:04 +0300

    Update data_preparation.py

--- src/inception/data_preparation.py
+++ src/inception/data_preparation.py
@@ -7,34 +7,38 @@ from keras.utils import np_utils
 def data_preparation(img_rows, img_cols):
     num_classes = 10
     # Load cifar10 training and validation sets
-    (X_train, Y_train), (X_valid, Y_valid) = cifar10.load_data()
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    print(X_train.shape)
+    print(X_test.shape)
 
     # Resize training images
     X_train = np.array([cv2.resize(img, (img_rows, img_cols)) for img in X_train[:, :, :, :]])
-    X_valid = np.array([cv2.resize(img, (img_rows, img_cols)) for img in X_valid[:, :, :, :]])
+    X_test = np.array([cv2.resize(img, (img_rows, img_cols)) for img in X_test[:, :, :, :]])
+    print(X_train.shape)
+    print(X_test.shape)
 
     # Transform targets to keras compatible format
-    Y_train = np_utils.to_categorical(Y_train, num_classes)
-    Y_valid = np_utils.to_categorical(Y_valid, num_classes)
+    y_train = np_utils.to_categorical(y_train, num_classes)
+    y_test = np_utils.to_categorical(y_test, num_classes)
 
     X_train = X_train.astype('float32')
-    X_valid = X_valid.astype('float32')
+    X_test = X_test.astype('float32')
 
     # preprocess data
     X_train = X_train / 255.0
-    X_valid = X_valid / 255.0
+    X_test = X_test / 255.0
 
-    return X_train, Y_train, X_valid, Y_valid
+    return X_train, y_train, X_test, y_test
 
 
 def data_preparation_transfer_learning():
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-    x_train = x_train / 255.0
-    x_test = x_test / 255.0
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
 
     y_train = np_utils.to_categorical(y_train, 10)
     y_test = np_utils.to_categorical(y_test, 10)
-    print(x_train.shape)
-    print(x_test.shape)
-    return x_train, y_train, x_test, y_test
+    print(X_train.shape)
+    print(X_test.shape)
+    return X_train, y_train, X_test, y_test
