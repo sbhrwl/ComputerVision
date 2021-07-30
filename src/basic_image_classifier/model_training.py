ba2d8a15cce762cef281129906cac992ba955852
sbhrwl <sabharwalonnet@gmail.com>
2021-07-30 18:43:56 +0300

    Update model_training.py

--- src/basic_image_classifier/model_training.py
+++ src/basic_image_classifier/model_training.py
@@ -7,11 +7,11 @@ def train_model():
     training_set_images, test_set_images = image_preprocessing()
     model = build_model()
     start = datetime.now()
-    model.fit_generator(training_set_images,
-                        steps_per_epoch=62,
-                        epochs=1,
-                        validation_data=test_set_images,
-                        validation_steps=2000)
+    model.fit(training_set_images,
+              steps_per_epoch=62,
+              epochs=1,
+              validation_data=test_set_images,
+              validation_steps=2000)
 
     duration = datetime.now() - start
     print("Training completed in time: ", duration)
