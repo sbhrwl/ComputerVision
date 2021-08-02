9aea875e51cbcadb37fa7a21a69c9068c90debc8
sbhrwl <sabharwalonnet@gmail.com>
2021-08-02 14:39:19 +0300

    inception refactored

--- src/inception/model_architectures.py
+++ src/inception/model_architectures.py
@@ -1,6 +1,7 @@
 import keras
 from keras.layers import Conv2D, Flatten, Dense, MaxPool2D, Dropout, Input, concatenate, AveragePooling2D
 from keras.models import Model
+from src.inception.model_architectures_tf import *
 
 
 def model_architectures():
@@ -167,57 +168,3 @@ def build_model_inception():
     model_with_classifiers = Model(input_layer, [x, classifier_1, classifier_2], name='googlenet_complete_architecture')
     model_with_classifiers.summary()
     return model_with_classifiers
-
-
-def inception_transfer_learning():
-    # @title Default title text
-    conv_base = InceptionV3(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
-    conv_base.summary()
-
-    model = models.Sequential()
-    # UpSampling increase the row and column of the data.
-    # Sometimes if we have less data so we can try to increase the data in this way.
-    model.add(UpSampling2D((2, 2)))
-    model.add(UpSampling2D((2, 2)))
-    model.add(UpSampling2D((2, 2)))
-
-    # conv_base is the inception network
-    model.add(conv_base)
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(BatchNormalization())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(BatchNormalization())
-    model.add(Dense(10, activation='softmax'))
-
-    input_shape = (None, 32, 32, 3)
-    model.build(input_shape)
-    model.summary()
-    return model
-
-
-def inception_transfer_learning_starting_from_mixed_7_layer():
-    # local_weights_file = path_inception
-    pre_trained_model = InceptionV3(input_shape=(128, 128, 3),
-                                    include_top=False,
-                                    weights='imagenet')
-    # pre_trained_model.load_weights(local_weights_file)
-
-    for layer in pre_trained_model.layers:
-        layer.trainable = False
-
-    pre_trained_model.summary()
-    last_layer = pre_trained_model.get_layer('mixed7')
-    print('last layer output shape: ', last_layer.output_shape)
-    last_output = last_layer.output
-
-    x = layers.Flatten()(last_output)
-    x = layers.Dense(1024, activation='relu')(x)
-    x = layers.Dropout(0.2)(x)
-    x = layers.Dense(10, activation='softmax')(x)
-
-    model = Model(pre_trained_model.input, x)
-    model.summary()
-    return model
--- src/inception/model_training.py
+++ src/inception/model_training.py
@@ -19,48 +19,22 @@ def decay(epoch):
     return learning_rate
 
 
-def start_scratch_training(model, X_train, y_train, X_validation, y_validation):
+def start_training(model, X_train, y_train, X_validation, y_validation, scratch_training):
     # Step 1: Compile model
-    model.compile(
-        loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'],
-        loss_weights=[1, 0.3, 0.3],
-        optimizer='sgd',
-        metrics=['accuracy'])
-
-    # Step 2: Setup Checkpoints
-    lr_sc = LearningRateScheduler(decay, verbose=1)
-    checkpoint = ModelCheckpoint(filepath='artifacts/model/inception/my_model.h5',
-                                 verbose=1, save_best_only=True)
-    callbacks = [checkpoint, lr_sc]
-
-    # Step 3: Setup Training parameters
-    batch_size = 256
-    epochs = 1  # 50
-
-    start = datetime.now()
-    history = model.fit(X_train,
-                        [y_train, y_train, y_train],
-                        validation_data=(X_validation, [y_validation, y_validation, y_validation]),
-                        epochs=epochs,
-                        batch_size=batch_size,
-                        callbacks=callbacks)
-
-    duration = datetime.now() - start
-    print("Training completed in time: ", duration)
-
-    # Step 5: Save Model
-    print("Model saved to disk via ModelCheckpoint callback")
-
-
-def start_training(model, X_train, y_train, X_validation, y_validation):
-    # Step 1: Compile model
-    # initial_learning_rate = 0.01
-    # sgd = SGD(lr=initial_learning_rate, momentum=0.9, nesterov=False)
-    model.compile(
-        loss='categorical_crossentropy',
-        loss_weights=[1, 0.3, 0.3],
-        optimizer='sgd',
-        metrics=['accuracy'])
+    if scratch_training == "Y":
+        model.compile(
+            loss=['categorical_crossentropy', 'categorical_crossentropy', 'categorical_crossentropy'],
+            loss_weights=[1, 0.3, 0.3],
+            optimizer='sgd',
+            metrics=['accuracy'])
+    else:
+        # initial_learning_rate = 0.01
+        # sgd = SGD(lr=initial_learning_rate, momentum=0.9, nesterov=False)
+        model.compile(
+            loss='categorical_crossentropy',
+            loss_weights=[1, 0.3, 0.3],
+            optimizer='sgd',
+            metrics=['accuracy'])
 
     # Step 2: Setup Checkpoints
     lr_sc = LearningRateScheduler(decay, verbose=1)
@@ -74,11 +48,19 @@ def start_training(model, X_train, y_train, X_validation, y_validation):
 
     # Step 4: Start Training
     start = datetime.now()
-    history = model.fit(X_train, y_train,
-                        validation_data=(X_validation, y_validation),
-                        epochs=1,
-                        batch_size=256,
-                        callbacks=callbacks)
+    if scratch_training == "Y":
+        print("Starting training from scratch")
+        history = model.fit(X_train, [y_train, y_train, y_train],
+                            validation_data=(X_validation, [y_validation, y_validation, y_validation]),
+                            epochs=epochs,
+                            batch_size=batch_size,
+                            callbacks=callbacks)
+    else:
+        history = model.fit(X_train, y_train,
+                            validation_data=(X_validation, y_validation),
+                            epochs=1,
+                            batch_size=256,
+                            callbacks=callbacks)
 
     duration = datetime.now() - start
     print("Training completed in time: ", duration)
@@ -89,10 +71,10 @@ def start_training(model, X_train, y_train, X_validation, y_validation):
 
 def model_preparation():
     train_features, train_labels, validation_features, validation_labels, test_features, test_labels = \
-        data_preparation_cifar_original()
+        data_preparation_cifar_resize(128, 128)
+    # data_preparation_cifar_resize(128,128), data_preparation_cifar_original()
     model = build_model()
-    # start_scratch_training(model, train_features, train_labels, validation_features, validation_labels)
-    start_training(model, train_features, train_labels, validation_features, validation_labels)
+    start_training(model, train_features, train_labels, validation_features, validation_labels, 'N')
 
 
 if __name__ == '__main__':
