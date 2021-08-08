c8e3bb4ea22cd25ac5270ea90d44e6d6abec9cb2
sbhrwl <sabharwalonnet@gmail.com>
2021-08-08 04:32:56 +0300

    refactored

--- src/core/setup_callbacks.py
+++ src/core/setup_callbacks.py
@@ -1,12 +1,13 @@
 import math
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, ReduceLROnPlateau
 from src.core.utils import get_parameters
 from src.core.plot_losses import PlotLosses
 
 
-def decay(epoch):
+def decay():
     config = get_parameters()
     lr_sc_config = config["callbacks"]["LearningRateScheduler"]
+    epoch = lr_sc_config["epoch"]
     initial_learning_rate = lr_sc_config["initial_learning_rate"]
     drop = lr_sc_config["drop"]
     epochs_drop = lr_sc_config["epochs_drop"]
@@ -22,6 +23,9 @@ def get_callbacks():
                                  verbose=1,
                                  save_best_only=True)
 
+    early_stopping_callback = EarlyStopping(patience=config["callbacks"]["early_stopping_callback"]["patience"],
+                                            restore_best_weights=True)
+
     lr_sc = LearningRateScheduler(decay, verbose=1)
 
     lrr_config = config["callbacks"]["ReduceLROnPlateau"]
@@ -32,4 +36,4 @@ def get_callbacks():
         min_lr=lrr_config["min_lr"])
 
     plot_losses = PlotLosses()
-    return [checkpoint, lr_sc, lrr, plot_losses]
+    return [checkpoint, early_stopping_callback, lr_sc, lrr, plot_losses]
